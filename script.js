15672396adf35d46887423aa11e36bbd4a0bfa40
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-07-12 10:41:41 +0530

    completed dom manipulation

---
+++ script.js
@@ -0,0 +1,11 @@
+const box=document.querySelector("#box");
+
+box.addEventListener('click',function(){
+    box.style.backgroundColor="yellow";
+    box.style.width="600px";
+    box.style.height="600px";
+})
+
+
+
+
